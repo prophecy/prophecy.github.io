e61e924f8d238ecf0c9f2579668b2e73ca211ade
prophecy <prophecyx@hotmail.com>
2020-03-23 18:01:00 +0700

    Twisted box shows the result in the high-precision canvas

--- three-chart/twisted_box.js
+++ three-chart/twisted_box.js
@@ -42,6 +42,17 @@ function twist(geometry, direction) {
   geometry.verticesNeedUpdate = true;
 }
 
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
 var direction = 1.0;
 var deltaTime = 0;
 var prevSec = 0;
@@ -50,6 +61,12 @@ var twistedTime = 0.0;
 
 // Render the frames
 function render(time) {
+
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
 
   // Get delta time 
   const curSec = time * 0.001;

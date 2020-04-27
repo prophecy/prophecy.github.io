05df96959d8d15303268f2cad95bc79b025bad31
Adawat <adawat.c@kbtg.tech>
2020-04-27 13:52:41 +0700

    Added glab-login web

---
+++ glab-login/precache-manifest.469c29cdb628aed0e72a210385ca63a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f825e092e97ebb94142483486fbbbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eaa20f5c02f05830ca8",
+    "url": "/static/css/main.5ee2ac5b.chunk.css"
+  },
+  {
+    "revision": "cdfe3e5152dfd690731f",
+    "url": "/static/js/2.96b3a7e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96b3a7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eaa20f5c02f05830ca8",
+    "url": "/static/js/main.84615940.chunk.js"
+  },
+  {
+    "revision": "db94bec3b643dc749801",
+    "url": "/static/js/runtime-main.e0082682.js"
+  },
+  {
+    "revision": "99b2a6e5cdc40f50e21cf84d5c2ec5f3",
+    "url": "/static/media/the_witcher_logo.99b2a6e5.png"
+  }
+]);
